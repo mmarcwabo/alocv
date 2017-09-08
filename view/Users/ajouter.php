70b850fa56c1134e5aed45c3cc60227ed08c35cd
Marcellin Dbfx <Marcellin Dbfx@DESKTOP-6J2QA7O>
2017-09-08 11:47:01 +0200

    Merge origin/master
    Conflicts:
    	README.md

--- view/Users/ajouter.php
+++ view/Users/ajouter.php
@@ -10,10 +10,10 @@
         <h1>Ajouter - Employé</h1>
         <span><?php echo (isset($_GET['param'])) ? $_GET['param'] : " " ?></span>
         <form action="../../controller/actionUsers/index.php?action=ajouter" method="POST">
-            <label for="nom" >Nom : </label><input type="text" name="nom"  /><br/>
-            <label for="prenom">Prenom : </label><input type="text" name="prenom"  /><br/>
-            <label for="login">Nom d'utilisateur : </label><input type="text" name="login" /><br/>
-            <label for="password">Mot de passe : </label><input type="password" name="password"/><br/>
+            <label for="nom" >Nom : </label><input type="text" name="nom" required /><br/>
+            <label for="prenom">Prenom : </label><input type="text" name="prenom" required /><br/>
+            <label for="login">Nom d'utilisateur : </label><input type="text" name="login" required/><br/>
+            <label for="password">Mot de passe : </label><input type="password" name="password" required/><br/>
             <label for="role">Rôle : </label>
             <select name="type">
                 <option>Admin</option>
